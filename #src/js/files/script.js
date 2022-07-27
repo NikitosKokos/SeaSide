135bdf1c033019aea7b4af429480c6712cc392ca
NikitosKokos <nikitaodessafreelance@gmail.com>
2022-07-27 23:14:03 +0300

    first commit

---
+++ #src/js/files/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.header__nav');
+    const header = document.querySelector('.header');
+   //  const nav = document.querySelector('.burger__close-icon');
+
+   const toggleMenu = () => {
+      nav.classList.toggle('active');
+      header.classList.toggle('active');
+      document.body.classList.toggle('_lock');
+   }
+
+   const headerScroll = () => {
+      if(this.scrollY >= 30){
+         header.classList.add('scroll');
+      }else{
+         header.classList.remove('scroll');
+      }
+   }
+
+   burger.addEventListener('click', toggleMenu);
+   window.addEventListener('scroll', headerScroll);
+
+}); // end
